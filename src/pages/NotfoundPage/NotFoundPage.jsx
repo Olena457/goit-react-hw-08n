0b262fb2e7145687fddf746309ac30c6a6ae1b0b
Olena457 <olenademianenko42@gmail.com>
2024-09-10 01:40:48 +0300

    corected2

--- src/pages/NotfoundPage/NotFoundPage.jsx
+++ src/pages/NotfoundPage/NotFoundPage.jsx
@@ -3,16 +3,12 @@ import css from './NotFoundPage.module.css';
 
 export default function NotFoundPage() {
   return (
-    <>
-      <div className={css.container}>
-        <>
-          <h1 className={css.text}>Page not found</h1>
-          <p className={css.subTitle}>Please go to home page</p>
-          <Link to="/" className={css.btn}>
-            Home page
-          </Link>
-        </>
-      </div>
-    </>
+    <div className={css.container}>
+      <h1 className={css.text}>Page not found</h1>
+      <p className={css.subTitle}>Please go to home page</p>
+      <Link to="/" className={css.btn}>
+        Home page
+      </Link>
+    </div>
   );
 }
--- src/store/auth/operationsAuth.js
+++ src/store/auth/operationsAuth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.goit.global';
+// axios.defaults.baseURL = 'https://connections-api.goit.global';
 
 // axios.defaults.baseURL = 'https://connections-api.herokuapp.com/api/';
 // axios.defaults.baseURL = '/api/';
