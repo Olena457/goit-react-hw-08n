f86b49648a948d9bccf0d3893e5cb43ffcc17cd7
Olena457 <olenademianenko42@gmail.com>
2024-09-09 20:18:23 +0300

    corect not found page

--- src/pages/NotfoundPage/NotFoundPage.jsx
+++ src/pages/NotfoundPage/NotFoundPage.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import css from './NotFoundPage.module.css';
+
 export default function NotFoundPage() {
   return (
     <div className={css.container}>
-      <h1 className={css.text} />
-      "Page not found "<h1 />
-      <h3 className={css.subTitle}>"Please to home page "</h3>
+      <h1 className={css.text}>Page not found</h1>
+      <h3 className={css.subTitle}>Please go to home page</h3>
       <Link to="/" className={css.btn}>
         Home page
       </Link>
