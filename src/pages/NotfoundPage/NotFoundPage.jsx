bfe27770cf1454b17238703dcc2fdb26288556f4
Olena457 <olenademianenko42@gmail.com>
2024-09-09 19:50:46 +0300

    Update NotFoundPage.jsx

--- src/pages/NotfoundPage/NotFoundPage.jsx
+++ src/pages/NotfoundPage/NotFoundPage.jsx
@@ -1,12 +1,10 @@
 import { Link } from 'react-router-dom';
-import { FcHighPriority } from 'react-icons/fc';
 import css from './NotFoundPage.module.css';
 export default function NotFoundPage() {
   return (
     <div className={css.container}>
       <h1 className={css.text} />
-      "Page not found
-      <FcHighPriority />"<h1 />
+      "Page not found "<h1 />
       <h3 className={css.subTitle}>"Please to home page "</h3>
       <Link to="/" className={css.btn}>
         Home page
