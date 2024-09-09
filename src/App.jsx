2e9c6127b3fbb4c207a20562b0ab7e05f3f8f277
Olena457 <olenademianenko42@gmail.com>
2024-09-10 02:46:29 +0300

    fixed all problem

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,6 @@ const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage.jsx'));
 const ContactsPage = lazy(() =>
   import('./pages/ContactsPage/ContactsPage.jsx')
 );
-// const NotFoundPage = lazy(() =>
-//   import('./pages/NotFoundPage/NotFoundPage.jsx')
-// );
 
 const App = () => {
   const dispatch = useDispatch();
