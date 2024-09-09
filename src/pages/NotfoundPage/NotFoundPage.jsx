3c8a9818f28830b88eef8d9b496a7485e908f237
Olena457 <olenademianenko42@gmail.com>
2024-09-09 19:27:35 +0300

    corected vercel

--- src/pages/NotfoundPage/NotFoundPage.jsx
+++ src/pages/NotfoundPage/NotFoundPage.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { FcHighPriority } from 'react-icons/fc';
-import { FcHome } from 'react-icons/fc';
 import css from './NotFoundPage.module.css';
 export default function NotFoundPage() {
   return (
@@ -8,10 +7,7 @@ export default function NotFoundPage() {
       <h1 className={css.text} />
       "Page not found
       <FcHighPriority />"<h1 />
-      <h3 className={css.subTitle}>
-        "Please to home page
-        <FcHome /> "
-      </h3>
+      <h3 className={css.subTitle}>"Please to home page "</h3>
       <Link to="/" className={css.btn}>
         Home page
       </Link>
