fc40da7354217f748018e7d28806b63d0f609f44
Olena457 <olenademianenko42@gmail.com>
2024-09-09 17:49:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,4 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-  server: {
-    port: 8080, // Зміна порту на 3000
-    proxy: {
-      '/api': {
-        target: 'https://connections-api.herokuapp.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
