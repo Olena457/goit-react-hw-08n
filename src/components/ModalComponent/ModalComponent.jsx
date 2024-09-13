3a7ad4ae6787eb12fec384baa4bf3b492aba865f
Olena457 <olenademianenko42@gmail.com>
2024-09-13 17:15:59 +0300

    fixed index

--- src/components/ModalComponent/ModalComponent.jsx
+++ src/components/ModalComponent/ModalComponent.jsx
@@ -1,106 +1,6 @@
-// import React from 'react';
-//
-
-//
-// const ModalComponent = ({ show, handleClose, handleDelete }) => {
-//   return (
-//     <>
-//       <Modal show={show}  onHide={handleClose}>
-//         <Modal.Header closeButton >
-//           <Modal.Title>Confirm Deletion</Modal.Title>
-//         </Modal.Header>
-
-//         <Modal.Body >
-//           Are you sure you want to delete this contact?
-//         </Modal.Body>
-
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleClose}>
-//             Cancel
-//           </Button>
-//           <Button variant="danger" onClick={handleDelete}>
-//             Delete
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </>
-//   );
-// };
-
-// export default ModalComponent;
-// ____________________________________________
-// import React from 'react';
-// import Modal from 'react-modal';
-
-// const customStyles = {
-//   content: {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//   },
-// };
-
-// Modal.setAppElement('#root');
-
-// const ModalComponent = ({ show, handleClose, handleDelete }) => {
-//   return (
-//     <Modal
-//       isOpen={show}
-//       onRequestClose={handleClose}
-//       style={customStyles}
-//       contentLabel="Confirm Deletion"
-//     >
-//       <h2>Confirm Deletion</h2>
-//       <div>Are you sure you want to delete this contact?</div>
-//       <div>
-//         <button onClick={handleClose}>Cancel</button>
-//         <button onClick={handleDelete}>Delete</button>
-//       </div>
-//     </Modal>
-//   );
-// };
-
-// export default ModalComponent;
-// ___________________________________________________________
-// import React from 'react';
-// import Modal from 'react-modal';
-// import css from './ModalComponent.module.css'; // Імпортуємо CSS-модуль
-
-// const ModalComponent = ({ show, handleClose, handleDelete }) => {
-//   return (
-//     <Modal
-//       isOpen={show}
-//       onRequestClose={handleClose}
-//       className={css.modalContent} // Використовуємо клас з CSS-модуля
-//       contentLabel="Confirm Deletion"
-//     >
-//       <h2>Confirm Deletion</h2>
-//       <div>Are you sure you want to delete this contact?</div>
-//       <div className={css.modalFooter}>
-//         <button
-//           className={`${css.btn} ${css.btnSecondary}`}
-//           onClick={handleClose}
-//         >
-//           Cancel
-//         </button>
-//         <button
-//           className={`${css.btn} ${css.btnDanger}`}
-//           onClick={handleDelete}
-//         >
-//           Delete
-//         </button>
-//       </div>
-//     </Modal>
-//   );
-// };
-
-// export default ModalComponent;
 import React from 'react';
 import Modal from 'react-modal';
-import css from './ModalComponent.module.css'; // Імпортуємо CSS-модуль
+import css from './ModalComponent.module.css';
 
 Modal.setAppElement('#root');
 
@@ -111,8 +11,6 @@ const ModalComponent = ({ show, handleClose, handleDelete }) => {
       onRequestClose={handleClose}
       className={`${css.modalContent} ${css.modalOverlay}`}
       contentLabel="Confirm Deletion"
-      // Використовуємо клас з CSS-модуля для модального вікна
-      // Використовуємо клас з CSS-модуля для бекдропу
     >
       <h2 className={css.title}>Confirm Deletion</h2>
       <div className={css.question}>
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,28 +1,3 @@
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import ContactForm from '../../components/ContactForm/ContactForm.jsx';
-// import { getAllContactsOperation } from './../../store/contacts/operationsContact.js';
-// import ContactsList from './../../components/ContactsList/ContactsList.jsx';
-// import Loading from './../../components/Loading/Loading.jsx';
-// import css from './ContactsPage.module.css';
-// import { selectLoading } from '../../store/contacts/selectorsContacts.js';
-
-// export default function ContactsPage() {
-//   const dispatch = useDispatch();
-//   const isLoading = useSelector(selectLoading);
-
-//   useEffect(() => {
-//     dispatch(getAllContactsOperation());
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       <h3 className={css.title}>Your contacts</h3>
-//       <ContactForm />
-//       {isLoading ? <Loading /> : <ContactsList />}
-//     </div>
-//   );
-// }
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ContactForm from '../../components/ContactForm/ContactForm.jsx';
